6dfa1c7fba11966f5e8e5f8852fb8363bbefcd4e
Rena Hajjar <rena.hajjar@ontario.ca>
2024-07-08 14:49:20 -0400

    customer model

---
+++ src/main/java/com/renaspring/springdemo/Customer.java
@@ -0,0 +1,69 @@
+package com.renaspring.springdemo;
+
+public class Customer {
+    private Integer id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    public Customer(Integer id, String name, String email, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Customer() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/main/java/com/renaspring/springdemo/SpringDemoApplication.java
+++ src/main/java/com/renaspring/springdemo/SpringDemoApplication.java
@@ -14,33 +14,4 @@ public class SpringDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
-
-	record GreetResponse(
-			String greet,
-			List<String> favLangs,
-			Person person
-	){}
-
-	record Person(String name, int age, double savings){}
-
-	@GetMapping("/greetme")
-	private GreetResponse greet() {
-		GreetResponse response = new GreetResponse(
-				"Hello",
-				List.of("Java", "Golang", "Javascript"),
-				new Person("Rena", 19, 6000)
-		);
-		return response;
-	}
-
-	@GetMapping("/whoami")
-	private Person whoiam() {
-		GreetResponse response = new GreetResponse(
-				"My name is",
-				List.of("Python, Rust, Ruby, Spark"),
-				new Person("Jed", 19, 30000)
-		);
-
-		return response.person();
-	}
 }
