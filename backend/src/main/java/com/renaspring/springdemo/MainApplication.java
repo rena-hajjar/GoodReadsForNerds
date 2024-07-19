80a0d55ec8655cf2e40f6cac18cb1684d431f9be
Rena Hajjar <rena.hajjar@ontario.ca>
2024-07-19 10:57:18 -0400

    yolo

--- backend/src/main/java/com/renaspring/springdemo/MainApplication.java
+++ backend/src/main/java/com/renaspring/springdemo/MainApplication.java
@@ -1,5 +1,6 @@
 package com.renaspring.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
--- backend/src/main/java/com/renaspring/springdemo/QuickStartConnection.java
+++ backend/src/main/java/com/renaspring/springdemo/QuickStartConnection.java
@@ -3,7 +3,7 @@ package com.renaspring.springdemo;
 public class QuickStartConnection {
 
     public static void main(String[] args) {
-        String uri = "mongodb+srv://21rsh8:<password>@bookcluster.cn4ctid.mongodb.net/?retryWrites=true&w=majority&appName=BookCluster";
+        String uri = "mongodb+srv://21rsh8:FunnyBunny56_@bookcluster.cn4ctid.mongodb.net/?retryWrites=true&w=majority&appName=BookCluster";
 
         
     }
