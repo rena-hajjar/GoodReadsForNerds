b7589b442b2bc8d62d375b9836ec5eef281d7be7
Rena Hajjar <rena.hajjar@ontario.ca>
2024-07-19 10:47:29 -0400

    start connecting to db except its not working

--- backend/src/main/java/com/renaspring/springdemo/MainApplication.java
+++ backend/src/main/java/com/renaspring/springdemo/MainApplication.java
@@ -32,17 +32,16 @@ public class MainApplication {
 			String dateStarted
 	) {}
 
-//	@PostMapping
-//	public void addBook(@RequestBody NewBookRequest request) {
-//		Book book = new Book();
-//		book.setTitle(request.title);
-//		book.setAuthor(request.author);
-//		book.setStatus(BookStatus.BACKLOGGED); // decide what the 3 things will be
-//		book.setDateStarted(request.dateStarted);
-//
-//		bookRepository.save(book);
-//	}
-//
+	@PostMapping
+	public void addBook(@RequestBody NewBookRequest request) {
+		Book book = new Book();
+		book.setTitle(request.title);
+		book.setAuthor(request.author);
+		book.setStatus(BookStatus.BACKLOGGED); // decide what the 3 things will be
+
+		bookRepository.save(book);
+	}
+
 //	@DeleteMapping("{customerId}")
 //	public void deleteCustomer(@PathVariable("customerId") Integer id) {
 //		bookRepository.deleteById(id);
---
+++ backend/src/main/java/com/renaspring/springdemo/QuickStartConnection.java
@@ -0,0 +1,10 @@
+package com.renaspring.springdemo;
+
+public class QuickStartConnection {
+
+    public static void main(String[] args) {
+        String uri = "mongodb+srv://21rsh8:<password>@bookcluster.cn4ctid.mongodb.net/?retryWrites=true&w=majority&appName=BookCluster";
+
+        
+    }
+}
